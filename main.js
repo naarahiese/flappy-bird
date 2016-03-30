1a8859e4ac1f303555b0234f777f838337432989
naarahiese <naarahiese@gmail.com>
2016-03-30 16:02:53 -0700

    Update main.js

--- main.js
+++ main.js
@@ -170,7 +170,10 @@ function main() {
         scoreSnd = game.add.audio('score');
         hurtSnd = game.add.audio('hurt');
         // Add controls
-        //game.input.onDown.add(flap);
+        game.input.onDown.add(flap);
+        //add keyboard controls
+        flapKey=game.input.keyboard.addKey (Phaser.Keyboard.SPACEBAR);
+        flapKey.onDown.add (flap);
         // Start clouds timer
         cloudsTimer = new Phaser.Timer(game);
         cloudsTimer.onEvent.add(spawnCloud);
